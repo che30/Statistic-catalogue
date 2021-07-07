55920a054574c4844abe7d7189c8079ad3512012
che <che.blancoo58@gmail.com>
2021-07-07 18:56:11 +0100

    Fix linters

--- src/tests/actions/MovieSearchDetails.test.js
+++ src/tests/actions/MovieSearchDetails.test.js
@@ -1,12 +1,14 @@
+import { Moviesearchdetails } from '../../actions';
 
-import { Moviesearchdetails } from "../../actions";
-describe('search movie method',() =>{
-  it('it returns a value passed to  it ',()=>{
+describe('search movie method', () => {
+  it('it returns a value passed to  it ', () => {
     const result = Moviesearchdetails('argument');
     expect(result).toEqual(result);
-    })
-     it('throws an error when called with no argument',() =>{
-     expect(Moviesearchdetails()).not.toEqual({type: 'SEARCH MOVIE',
-      movies: ''});
-     })
-})
+  });
+  it('throws an error when called with no argument', () => {
+    expect(Moviesearchdetails()).not.toEqual({
+      type: 'SEARCH MOVIE',
+      movies: '',
+    });
+  });
+});
--- src/tests/actions/SearchMovie.test.js
+++ src/tests/actions/SearchMovie.test.js
@@ -1,12 +1,14 @@
-import { SearchMovie } from "../../actions";
-describe('search movie method',() =>{
-  it('it returns a value passed to  it ',()=>{
+import { SearchMovie } from '../../actions';
+
+describe('search movie method', () => {
+  it('it returns a value passed to  it ', () => {
     const result = SearchMovie('argument');
     expect(result).toEqual(result);
-    })
-    it('throws an error when called with no argument',() =>{
-    expect(SearchMovie()).not.toEqual({type: 'SEARCH MOVIE',
-     movies: ''});
-    })
-})
-
+  });
+  it('throws an error when called with no argument', () => {
+    expect(SearchMovie()).not.toEqual({
+      type: 'SEARCH MOVIE',
+      movies: '',
+    });
+  });
+});
